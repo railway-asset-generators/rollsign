91a775f54cfc9c7e2b84a0e3f7c373719060a02d
ra <rayax86@gmail.com>
2018-03-25 22:53:06 +1100

    Modify `tslint.json` to suit project needs
    Required fixings are excecuted.

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,19 @@
 <template>
-  <div class="home">
-    <img src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js + TypeScript App"/>
-  </div>
+    <div class="home">
+        <img src="../assets/logo.png">
+        <HelloWorld msg="Welcome to Your Vue.js + TypeScript App"/>
+    </div>
 </template>
 
 <script lang="ts">
-import Vue from 'vue';
-import HelloWorld from '@/components/HelloWorld.vue'; // @ is an alias to /src
+    import HelloWorld from '@/components/HelloWorld.vue'; // @ is an alias to /src
+    import Vue from 'vue';
 
-export default Vue.extend({
-  name: 'home',
-  components: {
-    HelloWorld,
-  },
-});
+
+    export default Vue.extend({
+        name: 'home',
+        components: {
+            HelloWorld,
+        },
+    });
 </script>
--- tests/unit/HelloWorld.spec.ts
+++ tests/unit/HelloWorld.spec.ts
@@ -1,13 +1,15 @@
-import { expect } from 'chai';
 import { shallow } from '@vue/test-utils';
+import { expect } from 'chai';
+
 import HelloWorld from '@/components/HelloWorld.vue';
 
+
 describe('HelloWorld.vue', () => {
-  it('renders props.msg when passed', () => {
-    const msg = 'new message';
-    const wrapper = shallow(HelloWorld, {
-      propsData: { msg },
+    it('renders props.msg when passed', () => {
+        const msg = 'new message';
+        const wrapper = shallow(HelloWorld, {
+            propsData: { msg },
+        });
+        expect(wrapper.text()).to.include(msg);
     });
-    expect(wrapper.text()).to.include(msg);
-  });
 });
